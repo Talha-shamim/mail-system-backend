f386c09c66c96967edb8468e7ad4e11486ce9d44
amar567 <60555078+amar567@users.noreply.github.com>
2021-06-27 13:54:27 +0530

    mongo added

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv';
 import allRoutes from "./routers/router.js";
+
+
+// import from server config
+dotenv.config({ path: './config.env' })
 
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb://localhost/mail_system";
-const PORT = 4000;
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT
 
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useCreateIndex: true,
 		useFindAndModify: false
 	})
-	const connection = mongoose.connection
-	connection.once('open', () => {
-		console.log('atlas connection established successfully')
-	})
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Connection Established With MongoDB on port ${PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
 
 app.use("/mail-system", allRoutes);
 
